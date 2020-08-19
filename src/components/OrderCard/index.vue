88f790db23e56cfee6d24173e40b8aea65a809f3
姚晟 <yao_luolong@163.com>
2020-08-19 17:43:09 +0800

    订单列表页面开发

--- src/components/OrderCard/index.vue
+++ src/components/OrderCard/index.vue
@@ -1,8 +1,26 @@
 <template>
   <div class="order-card">
-    <div class="title">订单编号:{{ item.orderId }}</div>
-    <div class="content">内容</div>
-    <div class="operation">操作</div>
+    <div class="title">
+      订单编号:{{ item.orderId||20170528510001 }}
+      <span>￥{{ item.orderMoney||88.00 }}</span>
+    </div>
+    <div class="content">
+      <div class="items">
+        <ul>
+          <li v-for="(i,index) in item.items" :key="index">
+            商品名称*7
+            <span style="float:right">￥22.00</span>
+          </li>
+        </ul>
+      </div>
+      <div class="date">
+        <span>下单时间</span>2018年9月18日 18:20
+      </div>
+    </div>
+    <div class="operation">
+      <div class="status">状态</div>
+      <van-button type="primary" round size="small">主要按钮</van-button>
+    </div>
   </div>
 </template>
 
@@ -43,19 +61,51 @@ export default {
 @import '@/styles/colors';
 .order-card {
   overflow: hidden;
-  width: 100%;
   display: flex;
   flex-direction: column;
-  margin: 10px;
+  padding: 10px;
   .title,
   .content,
   .operation {
-    margin: 5px;
+    margin: 3px;
   }
   .title {
-    font-size: 16px;
+    font-size: 18px;
     font-weight: 600;
     color: $theme-color;
+    span {
+      float: right;
+      color: #ff6034;
+    }
+  }
+  .content {
+    .items {
+      background: #eeeeee;
+      padding: 10px;
+    }
+    .date {
+      margin: 10px 10px 0px 10px;
+      span {
+        margin-right: 10px;
+        color: #c8c9cc;
+      }
+    }
+  }
+  .operation {
+    text-align: right;
+    display: flex;
+    justify-content: space-between;
+    .status {
+      color: #e91e63;
+      font-size: 16px;
+      font-weight: 600;
+      margin-left: 10px;
+      text-align: right;
+      line-height: 32px;
+    }
+    button {
+      margin-right: 10px;
+    }
   }
 }
 </style>
