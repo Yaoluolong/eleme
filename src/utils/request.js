e0e506908f982d31077f1fb4d54149e191743247
姚洛龙 <yao_luolong@163.com>
2020-09-05 11:32:25 +0800

    token问题

--- src/utils/request.js
+++ src/utils/request.js
@@ -10,7 +10,7 @@ const service = axios.create({
 service.interceptors.request.use(
   config => {
     if (store.getters.token) {
-      config.headers['Authorization'] = `Bearer ${getToken()}`
+      config.headers['token'] = `Bearer ${getToken()}`
     }
     return config
   },
