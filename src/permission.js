646c2b3978f54aed60c0315a1e5cd793cd939323
姚洛龙 <yao_luolong@163.com>
2020-09-05 11:05:52 +0800

    登录联调

--- src/permission.js
+++ src/permission.js
@@ -20,28 +20,25 @@ router.beforeEach(async(to, from, next) => {
     if (to.path === '/login') {
       next({ path: '/' })
       NProgress.done()
+    } else {
+      next()
+      // } else {
+      //   try {
+      //     const { roles } = await store.dispatch('user/getInfo')
+
+      //     const accessRoutes = await store.dispatch('permission/generateRoutes', roles)
+
+      //     router.addRoutes(accessRoutes)
+
+      //     next({ ...to, replace: true })
+      //   } catch (error) {
+      //     await store.dispatch('user/resetToken')
+      //     Message.error(error || 'Has Error')
+      //     next(`/login?redirect=${to.path}`)
+      //     NProgress.done()
+      //   }
+      // }
     }
-    // else {
-    //   const hasRoles = store.getters.roles && store.getters.roles.length > 0
-    //   if (hasRoles) {
-    //     next()
-    //   } else {
-    //     try {
-    //       const { roles } = await store.dispatch('user/getInfo')
-
-    //       const accessRoutes = await store.dispatch('permission/generateRoutes', roles)
-
-    //       router.addRoutes(accessRoutes)
-
-    //       next({ ...to, replace: true })
-    //     } catch (error) {
-    //       await store.dispatch('user/resetToken')
-    //       Message.error(error || 'Has Error')
-    //       next(`/login?redirect=${to.path}`)
-    //       NProgress.done()
-    //     }
-    //   }
-    // }
   } else {
     if (whiteList.indexOf(to.path) !== -1) {
       next()
